6311cc188e1b53c58cd4943c1f7bbf01e529a2ca
Dmitry Kutsenko <d2emonium@gmail.com>
2017-09-20 09:28:23 +0300

    Subclasses

--- src/generator/space/galaxy.py
+++ src/generator/space/galaxy.py
@@ -15,43 +15,43 @@ class Galaxy(Generated):
         return "Galaxy: \"%s\"" % (self.generated_value)
 
 
-class GalaxyGenerator1(ListGenerator):
-    generated_class = Galaxy
-    galaxy_names = galaxy_names[:2]
-
-    @classmethod
-    def generate_value(cls):
-        choices = [cls.generate_value(l) for l in cls.galaxy_names]
-        return "%s %s" % (
-            choices[0],
-            choices[1],
-        )
-
-
-class GalaxyGenerator2(GalaxyGenerator1):
-    galaxy_names = [
-        galaxy_names[1],
-        galaxy_names[3],
-    ]
-
-
-class GalaxyGenerator3(GalaxyGenerator1):
-    galaxy_names = [
-        galaxy_names[2],
-        galaxy_names[3],
-    ]
-
-
-class GalaxyGenerator4(TemplatedGenerator):
-    template = "{c}{c}-{n}{n}"
-
-    
-class GalaxyGenerator5(GalaxyGenerator4):
-    template = "{c}{c}{c} {n}{n}{c}"
-
-
 class GalaxyGenerator(PercentedGenerator):
     generated_class = Galaxy
+    class GalaxyGenerator1(ListGenerator):
+        generated_class = Galaxy
+        galaxy_names = galaxy_names[:2]
+    
+        @classmethod
+        def generate_value(cls):
+            choices = [ListGenerator.generate_value(l) for l in cls.galaxy_names]
+            return "%s %s" % (
+                choices[0],
+                choices[1],
+            )
+    
+    
+    class GalaxyGenerator2(GalaxyGenerator1):
+        galaxy_names = [
+            galaxy_names[1],
+            galaxy_names[3],
+        ]
+    
+    
+    class GalaxyGenerator3(GalaxyGenerator1):
+        galaxy_names = [
+            galaxy_names[2],
+            galaxy_names[3],
+        ]
+    
+    
+    class GalaxyGenerator4(TemplatedGenerator):
+        template = "{c}{c}-{n}{n}"
+    
+        
+    class GalaxyGenerator5(GalaxyGenerator4):
+        template = "{c}{c}{c} {n}{n}{c}"
+
+
     subgenerators = {
         30: GalaxyGenerator1,
         50: GalaxyGenerator2,
