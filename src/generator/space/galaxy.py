ec7d2ed994d2effac7188b6bfb34d1bd31292379
Dmitry Kutsenko <d2emonium@gmail.com>
2017-09-20 09:14:47 +0300

    Updating galaxy generator

--- src/generator/space/galaxy.py
+++ src/generator/space/galaxy.py
@@ -1,64 +1,78 @@
 import random
 from .. import Generated, DataGenerator, GeneratorTemplate
+from .fixtures import galaxy_names
 
 
 class Galaxy(Generated):
+    @property
+    def title(self):
+        return self.generated_value
+
+    @title.setter
+    def title(self, title):
+        self.generated_value = title
+            
     def __repr__(self):
-        return "Galaxy: \"%s\"" % (self.generated_text)
+        return "Galaxy: \"%s\"" % (self.generated_value)
 
 
-class GalaxyTemplate(GeneratorTemplate):
-    @classmethod
-    def generate1(cls):
-        return cls.generate([
-            "data/galaxy/galaxy1.txt",
-            "data/galaxy/galaxy2.txt",
-        ])
+class GalaxyGenerator1(DataGenerator):
+    generated_class = Galaxy
+    galaxy_names = galaxy_names[:2]
 
     @classmethod
-    def generate2(cls):
-        return cls.generate([
-            "data/galaxy/galaxy2.txt",
-            "data/galaxy/galaxy-type.txt",
-        ])
+    def generate_value(cls):
+        choices = [random.choice(l) for l in cls.galaxy_names]
+        return "%s %s" % (
+            choices[0],
+            choices[1],
+        )
+
+
+class GalaxyGenerator2(GalaxyGenerator1):
+    galaxy_names = [
+        galaxy_names[1],
+        galaxy_names[3],
+    ]
+
+
+class GalaxyGenerator3(GalaxyGenerator1):
+    galaxy_names = [
+        galaxy_names[2],
+        galaxy_names[3],
+    ]
+
+
+class GalaxyGenerator4(GalaxyGenerator1):
+    template = "{c}{c}-{n}{n}"
 
     @classmethod
-    def generate3(cls):
-        return cls.generate([
-            "data/galaxy/galaxy3.txt",
-            "data/galaxy/galaxy-type.txt",
-        ])
+    def generate_value(cls):
+        return GeneratorTemplate.pregenerate(cls.template)
 
-    @classmethod
-    def generate4(cls):
-        return cls.pregenerate("{c}{c}-{n}{n}")
-
-    @classmethod
-    def generate5(cls):
-        return cls.pregenerate("{c}{c}{c} {n}{n}{c}")
+    
+class GalaxyGenerator5(GalaxyGenerator4):
+    template = "{c}{c}{c} {n}{n}{c}"
 
 
 class GalaxyGenerator(DataGenerator):
     generated_class = Galaxy
-
+    subgenerators = {
+        30: GalaxyGenerator1,
+        50: GalaxyGenerator2,
+        80: GalaxyGenerator3,
+        90: GalaxyGenerator4,
+        100: GalaxyGenerator5,
+    }
+    
     @classmethod
-    def generate4(cls):
-        return GalaxyTemplate.generate4()
-
-    @classmethod
-    def generate5(cls):
-        return GalaxyTemplate.generate5()
-
-    @classmethod
-    def generate_text(cls):
-        methods = {
-            30: GalaxyTemplate.generate1,
-            50: GalaxyTemplate.generate2,
-            80: GalaxyTemplate.generate3,
-            90: GalaxyTemplate.generate4,
-            100: GalaxyTemplate.generate5,
-        }
-        chance = random.randint(0, 100)
-        for c in sorted(methods):
+    def generator_by_chance(cls, chance=0):
+        for c in sorted(cls.subgenerators):
             if c >= chance:
-                return methods[c]()
+                return cls.subgenerators[c]
+
+    @classmethod
+    def generate_value(cls):
+        chance = random.randint(0, 100)
+        g = cls.generator_by_chance(chance)
+        return g.generate_value()
