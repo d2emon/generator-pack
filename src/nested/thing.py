7c04b67c6b25b1e3d6903afff55e3260e51a1319
Dmitry Kutsenko <d2emonium@gmail.com>
2018-07-12 18:03:35 +0300

    Parse generation data

--- src/nested/thing.py
+++ src/nested/thing.py
@@ -1,4 +1,48 @@
+import random
+
+
 THINGS = dict()
+
+
+class SubGenerator:
+    def __init__(self, data):
+        self.data = data
+        print("SUB GEN", data)
+        # if not instanceof(data, str):
+        #     data = Choose(data)
+        # value, probability, amount = get_data(data)
+        self.get_value(self.data)
+        print(self.value, self.amount, self.probability)
+
+    def get_value(self, value):
+        data = value.split(",")
+        self.amount = [1]
+
+        if len(data) > 1:
+            self.get_amount(data[1])
+
+        if len(data) > 1:
+            probability = self.get_probability(data[1])
+        else:
+            probability = self.get_probability("1")
+
+        if len(probability) > 1:
+            self.probability = probability[0]
+            self.amount = [1]
+        else:
+            self.probability = 100
+        self.value = data[0]
+
+    def get_amount(self, amount):
+        r = amount.split("-")
+        if len(r) > 1:
+            # return Rand(r[0], r[1])
+            self.amount = [int(r[0]), int(r[1])]
+        else:
+            self.amount = amount[0:0]
+
+    def get_probability(self, probability):
+        return (probability + "?").split("%")
 
 
 class Thing:
@@ -6,13 +50,24 @@ class Thing:
 
     def __init__(self, name, contains, namegen=None):
         self.name = name
-        self.contains = contains
+
+        self.contains = None
+        self.generators = []
+
+        self.parse(contains)
         self.namegen = namegen
         if (self.namegen is None):
             self.namegen = self.name
 
         THINGS[name] = self
         self.things_n += 1
+
+    def parse(self, data):
+        print("PARSE", self.name, data)
+        self.contains = data
+        self.generators = []
+        for d in data:
+            self.generators.append(SubGenerator(d))
 
     @classmethod
     def get_thing(cls, key):
@@ -36,6 +91,7 @@ class Thing:
         self.contains += to_concat
 
         self.contains = filter(lambda item: item, self.contains)
+
 
 # universe stuff
 Thing("multiverse",["universe,10-30"],["multiverse","lasagnaverse","doughnutverse","towelverse","baconverse","sharkverse","nestedverse","tastyverse","upverse","downverse","layerverse","clusterverse","metaverse","quantiverse","paraverse","epiverse","alterverse","hypoverse","dimensioverse","planiverse","pluriverse","polyverse","maniverse","stackoverse","antiverse","superverse","upperverse","maxiverse","megaverse","babyverse","tinyverse","retroverse","ultraverse","topoverse","otherverse","bubbleverse","esreverse","versiverse","'verse","cookieverse","grandmaverse"])
