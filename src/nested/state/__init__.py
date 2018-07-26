d3f2abadb09b5803188057a06c12ba41cce667f0
Dmitry Kutsenko <d2emonium@gmail.com>
2018-07-26 18:53:14 +0300

    Farm

--- src/nested/state/__init__.py
+++ src/nested/state/__init__.py
@@ -206,6 +206,40 @@ class ApartmentBuilding(Building):
     ]
 
 
+# farms
+class Farm(Thing):
+    child_generators = [
+        ChildGenerator("fire", probability=0.3),
+        ChildGenerator("house", (1, 3)),
+        ChildGenerator("farmer", (1, 4)),
+        ChildGenerator("field", (1, 8)),
+        ChildGenerator("horse", probability=30),
+        ChildGenerator("horse", probability=30),
+        ChildGenerator("horse", probability=30),
+        ChildGenerator("poultry", (0, 3)),
+        ChildGenerator("grain silo", (0, 2)),
+        ChildGenerator("barn", (0, 2)),
+        ChildGenerator("warehouse", (0, 2)),
+        ChildGenerator("storage shed", (0, 2))
+    ]
+
+
+# new Thing("field",["grain","insect,5%","bird,10%","bird,5%","haystack,30%"],["wheat field","corn field","soy field","rice field","oat field","peanut field","tomato field","grape field","barley field","canola field","rye field","flower field"]);
+# new Thing("farmer",[".person"],"*PERSON*| (farmer)");
+# new Thing("grain",["plant cell"]);
+# new Thing("grain silo",["metal","grain"]);
+# new Thing("warehouse",["worker,0-2","small mammal,8%","ghost,0.3%","machine,0-4",".building"]);
+# new Thing("barn",[".building"]);
+# new Thing("storage shed",[".building"]);
+# new Thing("haystack",["grain","insect,10%","needle,0.1%"]);
+# new Thing("needle",["metal"]);
+
+# new Thing("factory",["worker,2-12","machine,1-12","pipes,40%","cables,0-1","public bathroom,60%","warehouse,0-2",".building"],[["toy","chocolate","car","yoghurt","processed food","pork products","canned beef","juice","soda","shoe","textile","computer","weapon","hardware"],[" factory"]]);
+# new Thing("worker",[".person"],"*PERSON*| (worker)");
+
+# new Thing("public bathroom",[".room","person,10%","person,1%","sink,1-4","toilet,1-4","mirror,0-3"],"restroom");
+
+
 # rooms
 class Roof(Thing):
     child_generators = [
@@ -615,10 +649,7 @@ class VisitorCity(City):
 
 # ChildGenerator("battlefield", probability=10),
 
-# ChildGenerator("police station"),
-# ChildGenerator("fire department"),
 # ChildGenerator("museum", probability=40),
-# ChildGenerator("library", probability=60),
 # ChildGenerator("hospital"),
 # ChildGenerator("farm", (0, 3)),
 # ChildGenerator("factory", (1, 4)),
@@ -698,6 +729,8 @@ CONTENTS = [
     House,
     Apartment,
     ApartmentBuilding,
+
+    Farm,
 
     Building,
 
