74b0f60e0e530ebbf549e222debc15984ee82760
Dmitry Kutsenko <d2emonium@gmail.com>
2018-08-21 17:42:00 +0300

    Test chainmail

--- src/run_chainmail.py
+++ src/run_chainmail.py
@@ -1,21 +1,5 @@
 #! /usr/bin/python
-from rpg.games.chainmail import Opponent, PlayerStack, Battle
-from rpg.games.chainmail.battle.movesystem import MoveCounterMove
-from rpg.games.chainmail.terrain import generateTerrain
-
-
-def play(*args, **kwargs):
-    players = PlayerStack()
-    players.add(Opponent("First"))
-    players.add(Opponent("Second"))
-
-    terrain = generateTerrain()
-    for row in terrain:
-        print([t.name for t in row])
-
-    battle = Battle(MoveCounterMove(), players)
-    battle.process()
-
+from rpg.games.chainmail.game import play
 
 if __name__ == "__main__":
     play()
