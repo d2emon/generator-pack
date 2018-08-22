5f087aeeed3505836b5c439dc74032c1bba83df1
Dmitry Kutsenko <d2emonium@gmail.com>
2018-08-22 09:15:05 +0300

    Refactor utils

--- src/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-def load_lines(filename):
-    lines = []
-    with open(filename, "r", encoding='utf-8') as f:
-        lines = [line.strip() for line in f]
-    return lines
-
-
-def load_text(filename):
-    return "\n".join(load_lines(filename))
