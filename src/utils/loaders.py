b306b4f8212ef6ffd67691346310ec8d86ec12a2
Dmitry Kutsenko <d2emonium@gmail.com>
2018-08-24 09:48:26 +0300

    Add path to loader

--- src/utils/loaders.py
+++ src/utils/loaders.py
@@ -1,11 +1,19 @@
+import os
+
+
+ROOT_PATH = os.path.abspath(os.path.join(os.path.curdir, "../"))
+
+
 def load_lines(filename):
     """
     Load file as array of lines
     :param filename:
     :return:
     """
+    full_filename = os.path.join(ROOT_PATH, filename)
+
     lines = []
-    with open('../' + filename, "r", encoding='utf-8') as f:
+    with open(full_filename, "r", encoding='utf-8') as f:
         lines = [line.strip() for line in f]
     return lines
 
