a4c870885087e6f3051f9f97ee3aa3785835cf68
Dmitry Kutsenko <d2emonium@gmail.com>
2018-08-26 09:46:30 +0300

    Motto

--- src/generator/other/motto.py
+++ src/generator/other/motto.py
@@ -1,13 +1,6 @@
-from generator.generator import ListGenerator
-from generator.generator.generated import Generated
+from generator.generator.generated import ListGenerated
 from generator.generator.generator_data import FileData
 
 
-class Motto(Generated):
-    title = "Motto"
-
-
-class MottoGenerator(ListGenerator):
-    generated_class = Motto
-    data = { 'name': FileData("data/motto.txt") }
-    template = "{name}."
+class Motto(ListGenerated):
+    data = {'value': FileData("data/motto.txt")}
