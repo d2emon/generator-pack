8c3819425d8164b03b328b74fd1318b93aced32b
Dmitry Kutsenko <d2emonium@gmail.com>
2018-10-12 17:51:58 +0300

    Cyberpunk generator

---
+++ src/generator/_unknown/cyberpunk_quest.py
@@ -0,0 +1,522 @@
+class CyberpunkQuestGenerator:
+    @classmethod
+    def generate_quest_giver(cls):
+        """
+        a
+
+        1. WHITE COLLAR WORKING FOR
+        1. Artificial intelligence research institute
+        2. City-government office
+        3. Tax office
+        4. Cyberparts industry
+        5. Weapons factory
+        6. Bank
+        7. Waste treatment plant
+        8. Energy plant
+        9. Human rights
+        10. Drug manufacturer
+
+        2. SHADY LOOKING PERSON
+        1. More chrome than man
+        2. Street war vet
+        3. Covered with rags
+        4. Drug ruined addict
+        5. Living weapon (packed with gear, ninja, cyber-freak)
+        6. Member of a anti-corporation terrorist group
+        7. Spy for a megacorp
+        8. Wanderer from countrywastes
+        9. Mutant
+        10. Psychotic schizo
+
+        3. ARTIFICIAL INTELLIGENCE
+        1. Combat robot
+        2. Near human android
+        3. Mega computer
+        4. Facility
+        5. Controlled by aliens
+        6. From anti-megacorp resistance
+        7. Uploaded consciousness of an important deceased professor
+        8. Self-aware
+        9. Masqueraded as opposite it actually represents
+        10. Of alien origin
+
+        4. ROCK BAND
+        1. Anti-corporation
+        2. Pro-corporation
+        3. Uses more drugs than performs music
+        4. Harbinger
+        5. More chrome than human
+        6. Underground
+        7. Extremely popular
+        8. More money and fame than they can handle
+        9. One-man band
+        10. Programmed and computerized holograms and random music generator
+
+        5. HACKER, WHO IS
+        1. 24/7 plugged and lives in cyberspace
+        2. Member of cyberspace cult
+        3. Having weird, weird habits
+        4. Paralyzed neck down
+        5. Abusing experimental drugs
+        6. Famous
+        7. Just a kid
+        8. Captive in cyberspace
+        9. Using old-school warez & gear
+        10. Operating from space
+
+        6. StreetSecurityMilitia (SSM)
+        1. Fighting against street crimes
+        2. Taking bribery
+        3. Doing nothing of importance
+        4. Corrupted
+        5. Consists of concerned citizens
+        6. Drug abusive vets
+        7. More chrome than men
+        8. Trigger happy
+        9. Too timorous to act
+        10. Fighting against megacorp
+
+        7: MEGACORP
+        1. One of the biggest experimental drug manufacturer
+        2. Human experiments
+        3. Violating human rights
+        4. Forerunner of A.I.
+        5. More gunpower than anything
+        6. Ruthless who do absolutely anything they need to
+        7.Manufacture luxury vehicles
+        8.Cyberware, guns, mobile armors...
+        9. No one knows what they actually do
+        10. Operates from spacestation
+
+        8: DETECTIVE/PRIVATE EYE
+        1. Old drunk
+        2. Does only minor cases
+        3. Has lost his family, extremely vengeful
+        4. Bat-shit insane
+        5. For money does anything
+        6. Doesn't have a clue what' going on
+        7. On megacorp's payroll
+        8. Tries her best to be a good guy
+        9. Uses old-school guns
+        10. Always smokes a cigarette, always
+
+        9. REPORTER/NEWS ANCHOR
+        1. Easy to bribe to tell the story different
+        2. After the truth in spite of her own safety
+        3. Private underground channel
+        4. Has more than one dirty secret in her pocket just in case
+        5. Has personal small army to protect her in her assignments
+        6. No one cares of her reports, she's mind-blowing blonde bombshell
+        7. Operates outside the city
+        8. Has lost her memory, lives this moment and reports it
+        9. Owes tons of cash for material she has obtained from private sources
+        10.Is a spy for a foreign state
+
+        10.GANG MEMBER
+        1. Drug cartel
+        2. Human trafficking and prostitution
+        3. Weapons
+        4. Vandalism, mischief, civil disobedience, anarchy
+        5. Anti-corporation propaganda
+        6. Thugs
+        7. Corporate like mafia
+        8. Cyberpuffed psychos
+        9. Intruders
+        10. Hackers
+
+        :return:
+        """
+        return None
+
+    @classmethod
+    def generate_quest_type(cls):
+        """
+        b
+
+        1. PROTECT
+        1. Intel
+        2. Anarchist hideout
+        3. Dwelling house
+        4. Rock band
+        5. Power plant
+        6. Yourself/your gang
+        7. Anti-corporation individual
+        8. Biomechanical mutant child
+        9. Cyberware technology
+        10. Netrunner's secret data route
+
+        2. INTELLIGENCE OF
+        1. Megacorp HQ
+        2. Crooked officer
+        3. A.I. plans
+        4. New dangerous drug
+        5. War between anarchists and megacorp
+        6. Old sewers
+        7. Skypalace penthouse
+        8. Newschannel motives
+        9. Rockband
+        10. Hacker
+
+        3. ANNIHILATION
+        1. Megacorp CEO
+        2. More chrome than man gone insane and berserk
+        3. Megacorp HQ
+        4. Hacker messing up whole system for everyone and -thing
+        5. Powerplant for rich sector
+        6. Pro-megacorp reporter
+        7. Pro-megacorp rockband
+        8. Dangerous drug factory
+        9. Area's police force
+        10. Outer space satellite
+
+        4 . THIEVERY
+        1. Shit load of drugs
+        2. Rock band's musical data
+        3. Shit load of guns
+        4. Megacorp documents
+        5. Experimental cybertech
+        6. Foodstuff
+        7. Kidnap a person
+        8. Megacorp plans
+        9. Shit load of money
+        10. Cyberdata
+
+        5. RETRIEVE
+        1. Data
+        2. Drugs
+        3. Money
+        4. People
+        5. Guns
+        6. Vehicles
+        7. Blueprints
+        8. Intel
+        9. Equipment
+        10. Cyberware
+
+        6. RESCUE
+        1. Anarchist
+        2. Children
+        3. A.I.
+        4. Relative
+        5. Innocent
+        6. Megacorp bossman
+        7. Experimental drugs chemist
+        8. Crime boss
+        9. Militia officer
+        10. Hacker
+
+        7. BUY/SELL (50/50)
+        1. Drugs
+        2. Guns
+        3. Cyberware
+        4. Vehicles
+        5. Intel
+        6. Blueprints
+        7. People
+        8. Building
+        9. Experimental biological mutation enchancement juice
+        10. Freedom
+
+        8. CAPTURE
+        1. Rock band members
+        2. Megacorp boss
+        3. A.I.
+        4. Megacorp HQ
+        5. Section of cyberspace
+        6. Streets
+        7. Militia department
+        8. Street dealer
+        9. Orbiting satellite
+        10. Cyber-freak
+
+        9. HIDE
+        1. Important intel
+        2. Anarchist boss
+        3. Reporter in trouble
+        4. Illegal goods
+        5. Vehicle
+        6. Location from militia scanners
+        7. Your cyberware
+        8. Kids that are planned to be captured to work in powerplants
+        9. A.I. that has betrayed a megacorp
+        10. Yourself!
+
+        10. GUARD
+        1. Your own block
+        2. Reporter on a risky mission
+        3. Anarchist who are planning to blow up a megacorp HQ
+        4. Private cornerstore from thugs
+        5. Smugglers that traffic goods for the poor
+        6. Hacker as he tries to hack megacorp files
+        7. Powerplant from anarchists who want to blow it up
+        8. People from militia
+        9. This rare living and breathing animal
+        10. Earth orbiting satellite. They want to destroy it to stop it broadcating anti-corporation propaganda
+
+
+        :return:
+        """
+        return None
+
+    @classmethod
+    def generate_target(cls):
+        """
+        c
+
+        1. LOCATION
+        Roll table f to determine the target location
+
+        2. PERSON
+        1. Roll table a
+        2. Roll table a
+        3. Roll table a
+        4. Roll table e
+        5. Roll table e
+        6. Roll table e
+        7. Roll table g
+        8. Roll table g
+        9. Roll table g
+        10. Roll twice for two targets
+
+        3. MEGACORP
+        1. Sokotyo – manufacturer of high quality (and secretly addictive) consumer electronics
+        2. V3 – virtual reality recreational center
+        3. WarLogic – developer of new militia equipment and weapons
+        4. Somotholos – drug empire
+        5. Neotime Pops – clubs and entertainment
+        6. Panzerfist – combat sports and hand-to-hand traning, secretly human trafficking
+        7. Andromeda666 – space technology
+        8. Victory-10 – oppressess lower class trying to wipe them out with any means
+        9. Satin XXX – sexual stimulation
+        10. !FutureNow! - biomechanical research, human experiments, mutations, gene manipulation
+
+        4 . INTEL
+        1. Files proving that one megacorp uses humans for experimental cyberware
+        2. Cyberspace security protocol
+        3. Blueprints of important strategic points of the city for anarchists
+        4. Data of wasteland tribe
+        5. Newschannel feed
+        6. Documents showing how militia washes money
+        7. Location of criminal organization hideout's whereabouts
+        8. Megacorp CEO's home location
+        9. Drug factory's special formula for mind and body enchancing experimental new product
+        10. Spying satellite's coordinates
+
+        5. ITEM
+        1. One of the kind gilded and diamond covered cyber-hand. Too bad it is attached to someone
+        2. Best computer deck in the city
+        3. A crate full of big guns
+        4. Portable drug factory
+        5. Ingredients for DIY explosives
+        6. Expensive vehicle tuning parts
+        7. A cyberkey that will unlock any lock, mechanical and digital
+        8. Object d'art
+        9. Megacorp's computer server
+        10. Expensive junk fallen from space
+
+        6. DATA (50/50 DL OR DELETE)
+        1. Dirty secrets of megacorp CEO
+        2. 10,000,000 virtual credits
+        3. Future news not yet broadcasted
+        4. Militia security protocol
+        5. Software to control militia robots
+        6. Gang illegal and stolen goods locations on virtual map
+        7. GPS info of anarchist hideout
+        8. City's future plans
+        9. City councel's votes
+        10. THE TRUTH
+
+        7. ADVANCED TECHNOLOGY
+        1. Personal teleporter, needs an activated portable destination marker
+        2. Harness, when attached will enchange all physical human abilities and capabilities by 50%, but will kill the
+            subject if removed. Slowly will degenerate subject into torpor. Then he will be commanded like a robot by
+            roll c-3
+        3. Anti-gravity suit. Makes you float like there was no gravity!
+        4. X-ray sunglasses. Three intensity options – 1: see through clothes, 2: see through walls, 3: see cyberspace
+            lines on the thin air
+        5. Infinite battery. Can be attached on any device, but when attached, will melt on it and cannot be removed
+            without harming the device and destroying the battery
+        6. Random number generator. Will generate any number between 1 and infinity
+        7. High-tech battle tank
+        8. Wearable multi-tool gauntlet
+        9. Military auto-targeting system with fancy vision enchance effects
+        10. Some alien tech... no one knows what it does – yet!
+
+        8. POLITICAL CAMPAIGN
+        1. Anti-technology ideologists
+        2. Corrupted canditate
+        3. Augmented human supremacist party
+        4. New populist party is lynchng the current council
+        5. Megacorp CEO supporting martial law
+        6. Hacker activists
+        7. Pacifist chromeheads
+        8. People against robot cruelty
+        9. Environment savers
+        10. Savior cult
+
+        9. PARTY
+        1. Neon sex orgy
+        2. Excstacy rave
+        3. Cyberspace mayhem
+        4. Strict but weird dresscode
+        5. Tomatina carnival
+        6. Megacorp jugend meeting
+        7. Moshpit/wall of death
+        8. Wedding
+        9. Animal pit (sweepstake, booze, drugs, boxing, all nasty stuff)
+        10. Conventicle
+
+        10. WEIRD
+        1. Shipment of stuffed pink bunnies
+        2. Crate full of weird and even scary sex toys
+        3. Box full of bootleg holodisks of illegally recorded rock gigs
+        4. Crystal ball what actually is a stylized cyberspace deck
+        5. Huge tube with human-alien hybrid inside
+        6. Counterfeit credit cards (topped up) printer
+        7. Assortion of Halloween costumes
+        8. Bling bling car rims
+        9. Canned unicorn meat
+        10. Full documents of megacorp ownership. You don't have to own anything nor do any business to become a
+            megacorp!
+
+        :return:
+        """
+        return None
+
+
+    @classmethod
+    def generate_possession(cls):
+        """
+        d
+
+        1. UNKNOWN
+        Roll again to see who or what this “unknown” is, but the player characters don't know it before investigation.
+            You can use tables a, e or g to determine who has it.
+
+        2. STOLEN
+        Use location table f for its whereabouts, but the player characters don't know it before investigation.
+
+        3. MEGACORP
+        Roll table c-3
+
+        4 . GANG
+        1. Bikers
+        2. Junkies
+        3. Chromemuscles
+        4. Space-agers
+        5. Acid hippies
+        6. Anarchists
+        7. Mafia
+        8. Beggar Thugs United
+        9. Groupies of a rock band
+        10. Hackers (virtual criminals)
+
+        5. SECURED
+        1. Vault with complex mechanical lock systems
+        2. Vault with complex digital and virtual lock with dangerous defence programs
+        3. Guarded by a robot turret
+        4. Guarded by merchants
+        5. Behind laser nets
+        6. Hidden behind secret door/hatch
+        7. Camouflaged
+        8. At another location (roll table f where)
+        9. Counterfeit item, real one is elsewhere (roll table f where, and d again)
+        10. Space station
+
+        6. COMMONER
+        1. Normal family
+        2. Dude/tte who chills around listening to music from oversized headphones
+        3. Unemployed person
+        4. Shop owner
+        5. Autoparts dealer (legal)
+        6. Someone who tries his best to be invisible, gray mass
+        7. Taxi-driver
+        8. Skyscraper window washer – he's got a tent on the platform he's working from. Cost efficient
+        9. Trashman
+        10. Kid
+
+        7. ROLL TABLE a
+        But he's not the quest giver
+
+        8. ROLL TABLE e
+
+        9. ROLL TABLE g
+
+        10. QUEST GIVER HIM/HERSELF
+
+        :return:
+        """
+        return None
+
+
+    @classmethod
+    def generate_professions(cls):
+        """
+        e
+
+        1. STREET WARRIOR
+        1. Trentch coat street samurai
+        2. Shades is everything
+        3. Wild pink neon punk
+        4. More chrome than man
+        5. Tanktop, muscles, big guns
+        6. Silent, small, inconspicuous
+        7. Expensive suits
+        8. Pimp daddy with lepard design and big ass hat
+        9. Kid
+        10. Senior citizen
+
+        2. ROCKSTAR
+        1. Bald, leather, chains, rivets
+        2. Crust punk
+        3. Neonlights, neon colors, so bright must be seen from moon
+        4. Lazy skateboarder, wonder how those clothes stay on her
+        5. A.I. hologram
+        6. Enchanted with all kinds of cyber
+        7. Mysterious, hard to tell what she's like
+        8. Only in cyberspace
+        9. Robot. Not a slick android-type, but a bulky metal junk
+        10. Biopunk alien mutant lifeform
+
+        3. WHITE COLLAR
+        1. Megacorp officer
+        2. Drug factory supervizor
+        3. Weapons represantive
+        4. Jet set drug abuser
+        5. Slave dealer/pimp
+        6. Commercial block owner
+        7. One who decides who lives
+        and who dies
+        8. Dangerous business mogul
+        9. He wants to make things
+        right
+        10. Buys everything, sells
+        nothing – where does the
+        money come then?
+
+        4. MERCENARY
+        1. Megacorp dog
+        2. Dog of war
+        3. Gun for hire
+        4. Personal revenge tour
+        5. Wreck of a man
+        6. Forced with drugs
+        7. Anti-megacorp
+        8. Pro-megacorp
+        9. A.I. controlled by an outsider
+        10. Has very strange causes
+
+        5. REPORTER
+        1. Everyone's favorite virtual reality hologram
+        2. Crazy and goes in the middle of destruction
+        3. Every news he tells is a lie
+        4. Has no clue of news, because he is a supermodel and only there for his face
+        5. Megacorp hired propagandist
+        6. Drug addict
+        7. Has cyberweapons but hides them well
+        8. Is an anarchist spy
+        9. Is an ex merc
+
+        :return:
+        """
+        return None
