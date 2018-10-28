32d4dd162150625cc80beb1f44c0a75171943fda
Dmitry Kutsenko <d2emonium@gmail.com>
2018-10-28 22:54:11 +0300

    Simple voronoi

--- src/fixtures/space/__init__.py
+++ src/fixtures/space/__init__.py
@@ -294,3 +294,79 @@ class Universe(Named, Spheric):
             draw.ellipse(adjust(*s.point()), s.color)
 
         return image
+
+
+def parabola(focus, l):
+    def x(y):
+        xf, yf = focus
+        return ((y - yf) ** 2 + xf ** 2 - l ** 2) / (2 * (xf - l))
+    return x
+
+
+def distance(point1, point2):
+    dx = point2[0] - point1[0]
+    dy = point2[1] - point1[1]
+    return math.sqrt(dx ** 2 + dy ** 2)
+
+
+def voronoi(points, size):
+    shapes = [[] for _ in points]
+    for x in range(size[0]):
+        for y in range(size[1]):
+            p = x, y
+            d = [distance(p, point) for point in points]
+            if d[0] < d[1] and d[0] < d[2] and d[0] < d[3] and d[0] < d[4]:
+                shapes[0].append(p)
+            elif d[1] < d[0] and d[1] < d[2] and d[1] < d[3] and d[1] < d[4]:
+                shapes[1].append(p)
+            elif d[2] < d[0] and d[2] < d[1] and d[2] < d[3] and d[2] < d[4]:
+                shapes[2].append(p)
+            elif d[3] < d[0] and d[3] < d[1] and d[3] < d[2] and d[3] < d[4]:
+                shapes[3].append(p)
+            elif d[4] < d[0] and d[4] < d[1] and d[4] < d[2] and d[4] < d[3]:
+                shapes[4].append(p)
+    return shapes
+
+
+def draw_map(voronoi_map):
+    print(voronoi_map)
+
+
+def draw_voronoi():
+    def point_ellipse(point):
+        x, y = point
+        x1, x2 = x - 1, x + 1
+        y1, y2 = y - 1, y + 1
+        return x1, y1, x2, y2
+
+    size = 512, 512
+    points = [
+        (100, 100),
+        (356, 301),
+        (400, 65),
+        (324, 145),
+        (200, 399),
+    ]
+
+    v = voronoi(points, (512, 512))
+
+    image = Image.new('RGB', size)
+
+    draw = ImageDraw.Draw(image)
+    colors = (
+        (0, 0, 255),
+        (0, 255, 0),
+        (0, 255, 255),
+        (255, 0, 0),
+        (255, 0, 255),
+    )
+    for i, shape in enumerate(v):
+        for p in shape:
+            draw.ellipse(point_ellipse(p), colors[i])
+
+    for p in points:
+        draw.ellipse(point_ellipse(p), (255, 255, 255))
+
+    image.show()
+
+    return draw_map(v)
