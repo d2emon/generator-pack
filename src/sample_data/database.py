6fe6209edfd9a7e9ed14bc03d52f2f09c8018216
Dmitry Kutsenko <d2emonium@gmail.com>
2019-12-12 10:11:21 +0300

    Make DataItem class

--- src/sample_data/database.py
+++ src/sample_data/database.py
@@ -1,20 +1,22 @@
-__DATA = []
+class DataItem:
+    items = []
 
+    def __init__(self, group_id, value):
+        self.group_id = group_id
+        self.value = value
 
-def add_item(item):
-    __DATA.append(item)
+    @classmethod
+    def add_item(cls, item):
+        cls.items.append(item)
 
-
-def get_items(query=lambda item: True):
-    return (item for item in __DATA if query(item))
+    @classmethod
+    def get_items(cls, query=lambda item: True):
+        return (item for item in cls.items if query(item))
 
 
 def add_to_group(group_id, value):
-    add_item({
-        'group_id': group_id,
-        'value': value,
-    })
+    DataItem.add_item(DataItem(group_id, value))
 
 
 def get_from_group(group_id):
-    return get_items(lambda item: item.get('group_id') == group_id)
+    return (item.value for item in DataItem.get_items(lambda i: i.group_id == group_id))
