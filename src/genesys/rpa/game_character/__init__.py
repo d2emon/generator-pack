038209685e0a846d5ae8fce1aa42dd79572d1098
Dmitry Kutsenko <d2emonium@gmail.com>
2020-03-27 14:03:25 +0300

    Move rpa to models

--- src/genesys/rpa/game_character/__init__.py
+++ src/genesys/rpa/game_character/__init__.py
@@ -1,7 +1,5 @@
 from .generate import CharacterGenerator, PartyGenerator, generate_options
 
 from .game_character import add, delete, export_data, import_data, export_email, import_email, backup, restore
-from .experience import *
-from .party import *
 
 from .email.email import *
