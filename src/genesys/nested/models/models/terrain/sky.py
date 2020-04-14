c25cb45cf1be9cb9705d10132f0e7d23e048c4b4
Dmitry Kutsenko <d2emonium@gmail.com>
2020-04-14 20:15:05 +0300

    Add Life for sky

--- src/genesys/nested/models/models/terrain/sky.py
+++ src/genesys/nested/models/models/terrain/sky.py
@@ -1,7 +1,8 @@
 from genesys.nested.models.models.unknown import VisitorShip, Plane, Rocketship, Sprowseship
 from genesys.nested.models import Model
-from ..chemistry import Ice, Rock, WaterState, elements
-from ..biology import SkyLife, SpaceAnimal
+from ..chemistry import Gas, Ice, Rock, WaterState, elements
+from ..biology import Habitat
+# from ..biology import SkyLife, SpaceAnimal
 
 
 class Meteorite(Model):
@@ -13,10 +14,10 @@ class Meteorite(Model):
             yield elements['Fe'].probable(40)
 
 
-class Ozone(Model):
-    class ChildrenFactory(Model.ChildrenFactory):
-        def children_classes(self):
-            yield elements['O']
+class Ozone(Gas):
+    class Factory(Gas.Factory):
+        def children(self):
+            yield Gas.from_atoms('O')
 
 
 class Cloud(WaterState):
@@ -28,29 +29,56 @@ class Precipitation(WaterState):
         default = ['rain', 'snow', 'hail', 'mist', 'fog', 'drizzle', 'storm']
 
 
-class Sky(Model):
-    class ChildrenFactory(Model.ChildrenFactory):
-        def children_classes(self):
+class Sky(Habitat):
+    transport = Habitat.children_property(VisitorShip)
+    meteorites = Habitat.children_property(Meteorite)
+    precipitations = Habitat.children_property(Precipitation)
+    clouds = Habitat.children_property(Cloud)
+    gases = Habitat.children_property(Gas)
+
+    class Factory(Model.Factory):
+        @classmethod
+        def transport(cls):
             yield VisitorShip.probable(10)
+
+        @classmethod
+        def meteorites(cls):
             yield Meteorite.probable(3)
-            yield SkyLife
+
+        @classmethod
+        def life(cls):
+            # yield SkyLife
+            yield None
+
+        @classmethod
+        def precipitations(cls):
             yield Precipitation.probable(50)
             yield from Cloud.multiple(2, 8)
-            yield elements['O']
-            yield elements['C']
+
+        @classmethod
+        def clouds(cls):
+            yield Gas.from_atoms('O')
+            yield Gas.from_atoms('C')
             yield Ozone
+
+        def children(self):
+            yield from self.transport()
+            yield from self.meteorites()
+            yield from self.life()
+            yield from self.precipitations()
+            yield from self.clouds()
 
 
 class TerraformedSky(Sky):
-    class ChildrenFactory(Sky.ChildrenFactory):
-        def children_classes(self):
+    class Factory(Sky.Factory):
+        @classmethod
+        def transport(cls):
             yield from Plane.multiple(1, 8)
             yield Rocketship.probable(20)
-            yield from super().children_classes()
 
 
 class FutureSky(Sky):
-    class ChildrenFactory(Sky.ChildrenFactory):
-        def children_classes(self):
+    class Factory(Sky.Factory):
+        @classmethod
+        def transport(cls):
             yield from Sprowseship.multiple(1, 8)
-            yield from super().children_classes()
