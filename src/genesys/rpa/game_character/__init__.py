e1bea7d95b30cdbfddc82eb2b033ab64588c638e
Dmitry Kutsenko <d2emonium@gmail.com>
2022-03-22 18:53:23 +0300

    Remove unused import

--- src/genesys/rpa/game_character/__init__.py
+++ src/genesys/rpa/game_character/__init__.py
@@ -2,4 +2,4 @@ from .generate import CharacterGenerator, PartyGenerator, generate_options
 
 from .game_character import add, delete, export_data, import_data, export_email, import_email, backup, restore
 
-from .email.email import *
+# from .email.email import *
