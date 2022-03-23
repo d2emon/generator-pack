017dc2b117d67f3759700c1fb6b92776a1393bde
Dmitry Kutsenko <d2emonium@gmail.com>
2022-03-23 17:04:23 +0300

    Remove old tests

--- src/run_v1.py
+++ src/run_v1.py
@@ -1,4 +1,38 @@
-from v1.test.fng.names import test
+from v1.factories.fng.names import AlienNameFactory, AmazonNameFactory, AnansiNameFactory, AngelNameFactory, \
+    AnimalSpeciesNameFactory, AnimatronicNameFactory, AnimeNameFactory, AnthousaiNameFactory, \
+    ApocalypseNicknameFactory, ArtificialIntelligenceNameFactory, BanditNameFactory, BansheeNameFactory, \
+    BarbarianNameFactory
+
+
+def show_multiple(name, generated, factoryClass):
+    print(name)
+    for value in generated:
+        print(f"\t{value}")
+
+
+def build_all():
+    factories = [
+        AlienNameFactory,
+        AmazonNameFactory,
+        AnansiNameFactory,
+        AngelNameFactory,
+        AnimalSpeciesNameFactory,
+        AnimatronicNameFactory,
+        AnimeNameFactory,
+        AnthousaiNameFactory,
+        ApocalypseNicknameFactory,
+        ArtificialIntelligenceNameFactory,
+
+        # BanditNameFactory,
+        # BansheeNameFactory,
+        # BarbarianNameFactory,
+    ]
+
+    for factoryClass in factories:
+        factory = factoryClass()
+        generated = factory.build10()
+        show_multiple(factoryClass.__name__, generated, factoryClass)
+
 
 if __name__ == "__main__":
-    test()
+    build_all()
