6564074b273aa7debf6bf18ff48d6aba477a5bc0
Dmitry Kutsenko <d2emonium@gmail.com>
2022-04-01 11:19:59 +0300

    Refactor encounter factory

--- src/genesys/storm/encounter/factories/encounter.py
+++ src/genesys/storm/encounter/factories/encounter.py
@@ -15,18 +15,18 @@ class EncounterFactory:
         self.daily_event_factory = DailyEventFactory(provider=provider)
         self.nightly_event_factory = NightlyEventFactory(provider=provider)
 
-    def __add_event(self):
+    def event_factory(self):
         dice1, dice2 = Dice(count=2).roll()
 
         yield self.nightly_event_factory() if dice1 % 2 else self.daily_event_factory()
 
         if self.roll_on_double and (dice1 == dice2):
-            yield from self.__add_event()
+            yield from self.event_factory()
 
-    def __events(self):
+    def events(self):
         yield self.daily_event_factory()
         yield self.nightly_event_factory()
-        yield from self.__add_event()
+        yield from self.event_factory()
 
     def __call__(self, day_id, *args, **kwargs):
-        return Day(day_id, *self.__events())
+        return Day(day_id, *self.events())
--- src/tests/genesys/storm/test_encounter_factory.py
+++ src/tests/genesys/storm/test_encounter_factory.py
@@ -1,74 +1,38 @@
 import random
 import unittest
-from dice.dice import Dice
-from genesys.storm.encounter.factories.distance import DistanceFactory
 from genesys.storm.encounter.factories.encounter import EncounterFactory
-from genesys.storm.encounter.factories.event import DailyEventFactory, NightlyEventFactory
-from models.encounters.distance import Distance
+from models.encounters.events.event import Event, DailyEvent, NightlyEvent
 from models.history.day import Day
-
-
-"""
-    def __add_event(self):
-        dice1, dice2 = Dice(count=2).roll()
-
-        yield self.nightly() if dice1 % 2 else self.daily()
-
-        if self.roll_on_double and (dice1 == dice2):
-            yield from self.__add_event()
-
-    def __events(self):
-        yield self.daily()
-        yield self.nightly()
-        yield from self.__add_event()
-
-    def __call__(self, day_id, *args, **kwargs):
-        return Day(day_id, *self.__events())
-"""
-
-
-class DistanceData:
-    def __init__(self):
-        self.dice = Dice()
 
 
 class TestEncounterFactory(unittest.TestCase):
     def setUp(self) -> None:
-        self.values = [random.uniform(0, 100) for _ in range(10)]
+        self.day_id = random.randint(1, 10)
+        self.factory = EncounterFactory()
 
-    def test_encounter_factory(self):
-        day_id = random.randint(1, 10)
+    def test_event_factory(self):
+        for event in self.factory.event_factory():
+            self.assertIsInstance(event, Event)
 
-        factory_1 = EncounterFactory(roll_on_double=True)
-        self.assertTrue(factory_1.roll_on_double)
-        self.assertIsInstance(factory_1.daily, DailyEventFactory)
-        self.assertIsInstance(factory_1.nightly, NightlyEventFactory)
+    def test_events(self):
+        events = list(self.factory.events())
+        self.assertGreaterEqual(len(events), 3)
+        for event in events:
+            self.assertIsInstance(event, Event)
 
-        day_1 = factory_1(day_id)
-        self.assertIsInstance(day_1, Day)
-        self.assertEqual(day_1.day_id, day_id)
+    def test_encounter_factory_roll_on_double(self):
+        factory = EncounterFactory(roll_on_double=True)
+        day = factory(self.day_id)
+        self.assertIsInstance(day, Day)
+        self.assertEqual(day.day_id, self.day_id)
+        self.assertGreaterEqual(len(day.events), 3)
 
-        factory_2 = EncounterFactory(roll_on_double=False)
-        self.assertFalse(factory_2.roll_on_double)
-        self.assertIsInstance(factory_2.daily, DailyEventFactory)
-        self.assertIsInstance(factory_2.nightly, NightlyEventFactory)
-
-        day_2 = factory_1(day_id)
-        self.assertIsInstance(day_2, Day)
-        self.assertEqual(day_2.day_id, day_id)
-
-    def test_distance_factory(self):
-        empty_factory = DistanceFactory(None)
-        self.assertIsNone(empty_factory())
-
-        data = DistanceData()
-        factory = DistanceFactory(data)
-        distance = factory()
-        self.assertIsInstance(distance, Distance)
-        # return Distance(
-        #     self.distance,
-        #     self.distance.dice.total(),
-        # )
+    def test_encounter_factory_dont_roll_on_double(self):
+        factory = EncounterFactory(roll_on_double=False)
+        day = factory(self.day_id)
+        self.assertIsInstance(day, Day)
+        self.assertEqual(day.day_id, self.day_id)
+        self.assertEqual(len(day.events), 3)
 
 
 if __name__ == "__main__":
