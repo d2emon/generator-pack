4bae647c7d96c038a39a1249d4a8682c31338df5
Dmitry Kutsenko <d2emonium@gmail.com>
2022-04-06 17:08:01 +0300

    Remove unused packages

--- src/models/v4/interface/named.py
+++
@@ -1,9 +0,0 @@
-class NamedModelInterface:
-    @property
-    def name(self):
-        """
-        Get next name from factory.name_factory
-
-        :return: Model name
-        """
-        raise NotImplementedError()
