dea613060ada7fb7c28eb3a399ba46883e798535
Dmitry Kutsenko <d2emonium@gmail.com>
2023-02-24 13:42:07 +0300

    Add other views for and_why

--- src/api/views/and_why.py
+++ src/api/views/and_why.py
@@ -1,5 +1,8 @@
 from flask import jsonify
 from genesys.and_why.factories.clothing import ClothingFactory
+from genesys.and_why.factories.doll import DollFactory
+from genesys.and_why.factories.gender import GenderFactory
+from genesys.and_why.factories.slot import SlotFactory
 from ..app import app
 
 
@@ -11,8 +14,9 @@ def get_index():
     ])
 
 
+@app.route('/api/v1.0/and_why/clothing')
 @app.route('/api/v1.0/and_why/clothing/<gender>')
-def get_clothing(gender):
+def get_clothing(gender=None):
     factory = ClothingFactory()
     models = factory(gender=gender)
 
@@ -24,24 +28,24 @@ def get_clothing(gender):
 
 
 @app.route('/api/v1.0/and_why/doll')
-def get_doll():
-    return jsonify([
-        'doll',
-        'clothing',
-    ])
+@app.route('/api/v1.0/and_why/doll/<gender>')
+def get_doll(gender=None):
+    factory = DollFactory()
+    model = factory(gender=gender)
+    return jsonify(model.values)
 
 
 @app.route('/api/v1.0/and_why/gender')
-def get_gender():
-    return jsonify([
-        'doll',
-        'clothing',
-    ])
+@app.route('/api/v1.0/and_why/gender/<gender>')
+def get_gender(gender=None):
+    factory = GenderFactory()
+    model = factory()
+    return jsonify(model)
 
 
 @app.route('/api/v1.0/and_why/slot')
 def get_slot():
-    return jsonify([
-        'doll',
-        'clothing',
-    ])
+    factory = SlotFactory()
+    models = factory()
+    result = list(models)
+    return jsonify(result)
