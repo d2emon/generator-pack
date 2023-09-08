408300aeec9929b1b8116a39af5ff945d532b294
Dmitry Kutsenko <d2emonium@gmail.com>
2023-09-08 19:02:51 +0300

    Add factories to summary

--- src/genesys/__init__.py
+++ src/genesys/__init__.py
@@ -3,34 +3,63 @@ Contents:
 
 * and_why
   * Generates character clothing from magazine
+  * Use ArrayDatabase
+  * Use DataItemDatabase
+  * Use ListFactory
+  * Use DataItemFactory
+  * Use ModelFactory
 * campsite
   * Generate campsite description
+  * Use ModelFactory
 * cp2020
   * Generate quest for cp2020
+  * Unready
 * demographics
   * Generate fantasy demographics
 * dndspeak
   * Generate encounters with description
+  * Use DataItemDatabase
+  * Use DataItemFactory
 * encounters
   * Generate encounters with description
+  * Use ModelFactory
 * fng
   * Generate fantasy names
+  * Use ListFactory
+  * Use TemplateFactory
+  * Use ModelFactory
+  * Use DataFactory
+  * Use DbFactory
 * lexicon
   * Generate topics for lexicon game
 * markov
   * Generate from Markov chain
+  * Use MarkovFactory
 * media
   * Generate media charts
 * nested
   * Generate nested data
+  * Use NestedFactory
 * redlands
   * Generate characters for redlands
+  * Use TemplateFactory
+  * Use ModelFactory
+  * Use ListModelFactory
 * scales
   * Generate item by scale
 * storm
   * Generators from stormtower magazine
+  * Use CSVDatabase
+  * Use JSONDatabase
+  * Use ListFactory
+  * Use ModelFactory
 * timeline
   * Generate world timeline
+  * Use ListFactory
+  * Use DiceFactory
 * world
   * Generate world from list
+  * Use ListFactory
+  * Use ModelFactory
+
 """
