d90dfa1ad42d27fb8bec8a4de5bfb878fc94d24f
Dmitry Kutsenko <d2emonium@gmail.com>
2023-09-08 17:35:17 +0300

    Add genesys contents

---
+++ src/genesys/__init__.py
@@ -0,0 +1,36 @@
+"""
+Contents:
+
+* and_why
+  * Generates character clothing from magazine
+* campsite
+  * Generate campsite description
+* cp2020
+  * Generate quest for cp2020
+* demographics
+  * Generate fantasy demographics
+* dndspeak
+  * Generate encounters with description
+* encounters
+  * Generate encounters with description
+* fng
+  * Generate fantasy names
+* lexicon
+  * Generate topics for lexicon game
+* markov
+  * Generate from Markov chain
+* media
+  * Generate media charts
+* nested
+  * Generate nested data
+* redlands
+  * Generate characters for redlands
+* scales
+  * Generate item by scale
+* storm
+  * Generators from stormtower magazine
+* timeline
+  * Generate world timeline
+* world
+  * Generate world from list
+"""
